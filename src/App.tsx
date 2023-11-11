1caa38c4455a0409cd61bc65cc7ddab1b2ca821e
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-11-11 21:01:44 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
-// import {
-//   Home,
-//   Explore,
-//   Saved,
-//   CreatePost,
-//   Profile,
-//   EditPost,
-//   PostDetails,
-//   UpdateProfile,
-//   AllUsers,
-// } from "@/_root/pages";
+import {
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import SignupForm from "./_auth/forms/SignupForm";
@@ -22,7 +22,7 @@ import { Toaster } from "@/components/ui/toaster";
 
 
 import "./globals.css";
-import Home from "./_root/pages/Home";
+// import Home from "./_root/pages/Home";
 
 const App = () => {
   return (
@@ -37,14 +37,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
-          {/* <Route path="/explore" element={<Explore />} />
+          <Route path="/explore" element={<Explore />} />
           <Route path="/saved" element={<Saved />} />
           <Route path="/all-users" element={<AllUsers />} />
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/update-post/:id" element={<EditPost />} />
           <Route path="/posts/:id" element={<PostDetails />} />
           <Route path="/profile/:id/*" element={<Profile />} />
-          <Route path="/update-profile/:id" element={<UpdateProfile />} /> */}
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
 
       </Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -59,5 +59,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 };
